9bab8832aacb5a87b75d87ceb2e10f6f1ed54835
eloise87 <eloise.marie@laposte.net>
2019-12-04 20:27:06 +0100

    probably final commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+
+    return (
+        <div className="container">
+            <button className="playButton"> Play</button>
+            <h1 className="title"> </h1>
+            <img src="https://image.noelshack.com/fichiers/2019/49/3/1575471833-rocket-rising-1.png" />
+        </div>
+    )
+}
+
+
+export default LandingPage;
