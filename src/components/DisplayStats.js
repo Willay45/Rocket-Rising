18f5d54fda3d51db1419f98e697d8c3f8c861a4e
PierreWCS <pierrelegrain45@gmail.com>
2019-12-05 11:36:18 +0100

    changing class names

--- src/components/DisplayStats.js
+++ src/components/DisplayStats.js
@@ -8,11 +8,11 @@ const DisplayStats = ({starters}) => {
         console.log(thePokemon);
     };
     return (
-        <div className="contenair">
+        <div className="container">
             <h1 className="pokeName">{starters.name}</h1>
             <p>{starters.description}</p>
             {starters.name ? <img src={starters.imgType} alt="" /> : null}
-            {starters.name ? <button className="theButton" onClick={chooseOne}>I want this pokemon !</button> : null}
+            {starters.name ? <button className="selectedPokemon" onClick={chooseOne}>I want this pokemon !</button> : null}
         </div>
     )
 };
