fa5667cc3c625b2ab9567e555f528bbf45b30ee3
PierreWCS <pierrelegrain45@gmail.com>
2019-12-05 17:09:20 +0100

    using json to get the pokemons info

--- src/components/DisplayStats.js
+++ src/components/DisplayStats.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './DisplayStats.css';
 
 const DisplayStats = ({starters}) => {
+    const [image, setImage] = useState('');
+
+    useEffect(() => {
+        if (starters.imgType) {
+            setImage(require(starters.imgType));
+        }
+    }, []);
+
     const choosenPokemon = [];
     const chooseOne = () => {
         choosenPokemon.push(starters);
-        console.log(choosenPokemon);
     };
+    console.log(image);
     return (
         <div className="container">
             <h1 className="pokeName">{starters.name}</h1>
             <p>{starters.description}</p>
-            {starters.name ? <img src={starters.imgType} alt="" /> : null}
-            {starters.name ? <button className="selectedPokemon" onClick={chooseOne}>I want this pokemon !</button> : null}
+            {starters.name ? <img src={image} alt="" /> : null}
+            {starters.name ? <button className="selectedPokemonButton" onClick={chooseOne}>I want this pokemon !</button> : null}
         </div>
     )
 };
