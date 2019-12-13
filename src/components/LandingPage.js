c9cbf37bd259797cba20ce95a9f3773d77fa4de0
eloise87 <eloise.marie@laposte.net>
2019-12-13 11:07:48 +0100

    modification of the home page's title

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ function LandingPage() {
     return (
         <div className="container">
             <button className="playButton"> Play</button>
-            <img src="https://image.noelshack.com/fichiers/2019/49/3/1575471833-rocket-rising-1.png" alt="home page picture" />
+            <img className="title" src="https://image.noelshack.com/fichiers/2019/49/3/1575471833-rocket-rising-1.png" alt="home page picture" />
         </div>
     )
 }
