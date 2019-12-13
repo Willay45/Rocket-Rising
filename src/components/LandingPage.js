10802d284554042269be898c73a0845264c03c90
eloise87 <eloise.marie@laposte.net>
2019-12-13 10:30:13 +0100

    modifications PR

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,8 +6,8 @@ function LandingPage() {
     return (
         <div className="container">
             <button className="playButton"> Play</button>
-            <h1 className="title"> </h1>
-            <img src="https://image.noelshack.com/fichiers/2019/49/3/1575471833-rocket-rising-1.png" />
+
+            <img id="title" src="https://image.noelshack.com/fichiers/2019/49/3/1575471833-rocket-rising-1.png" />
         </div>
     )
 }
