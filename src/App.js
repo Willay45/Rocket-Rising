53fcdbcbbd92a3bc5cfa3de43b4e87915b12aaa0
PierreWCS <pierrelegrain45@gmail.com>
2019-12-13 18:04:45 +0100

    fixes css bugs in starter pokemon

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import {Route, Switch} from 'react-router-dom';
 import NavMaps from "./components/NavMaps";
 import MapRender from "./components/MapRender";
+import StarterPokemon from "./components/StarterPokemon";
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact path="/map" component={NavMaps}/>
+                <Route path="/starter-pokemon" component={StarterPokemon}/>
                 <Route path="/map/:map" component={MapRender}/>
             </Switch>
         </div>
--- src/components/StarterPokemon.js
+++ src/components/StarterPokemon.js
@@ -11,7 +11,7 @@ const StarterPokemon = () => {
     };
 
     return (
-        <div className="container">
+        <div className="containerStarter">
             <h1 className="titleChoose">Choose your pokemon !</h1>
             <div className="threePokemons">
                 <img className="pokemon"
@@ -19,17 +19,18 @@ const StarterPokemon = () => {
                      src={starterPack.starters.ekans.img}
                      alt="ekans"
                 />
-                <img className="pokemon" src={starterPack.starters.gastly.img} onClick={() => {
-                    displayPokemon("gastly")
-                }}
-                     alt="gastly"/>
-                <img className="pokemon" src={starterPack.starters.bellsprout.img} onClick={() => {
-                    displayPokemon("bellsprout")
-                }}
-                     alt="bellsprout"/>
-                <DisplayStats
-                   starters={pokemonStat}/>
+                <img className="pokemon"
+                     src={starterPack.starters.gastly.img}
+                     onClick={() => displayPokemon("gastly")}
+                     alt="gastly"
+                />
+                <img className="pokemon"
+                     src={starterPack.starters.bellsprout.img}
+                     onClick={() => displayPokemon("bellsprout")}
+                     alt="bellsprout"
+                />
             </div>
+            <DisplayStats starters={pokemonStat}/>
         </div>
     )
 };
