c05985e6c6df09a06152841fb35a6edfd1bf7bbc
PierreWCS <pierrelegrain45@gmail.com>
2019-12-17 16:28:33 +0100

    alt attributes for the fake navigation and model of pokemonarray for test in an other dedicated file

--- src/components/TeamPokemon.js
+++ src/components/TeamPokemon.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from 'react';
 import './TeamPokemon.css';
 import axios from 'axios';
 import DisplayPokemonTeamStats from './DisplayPokemonTeamStats';
-import PokemonCard from "./PokemonCard";
+import PokemonCard from './PokemonCard';
+import model from './modelPokemons';
+
 
 const TeamPokemon = () => {
     const [pokemonTeam, setPokemonTeam] = useState([]);
@@ -20,99 +22,6 @@ const TeamPokemon = () => {
         setIndexSelected(index);
         setPokemonStat(selectedPokemon);
     };
-
-    let model = [
-        {
-            name: "pikachu",
-            type: "electric",
-            stats:
-                [
-                    {base_stat: 88},
-                    {base_stat: 24},
-                    {base_stat: 48},
-                    {base_stat: 80},
-                    {base_stat: 35},
-                    {base_stat: 58},
-                    {base_stat: 58},
-                ],
-            isFavorite: false,
-            lvl: 42,
-            hp: 121,
-            hpMax: 121
-        },
-        {
-            name: "charizard",
-            type: "fire",
-            stats:
-                [
-                    {base_stat: 88},
-                    {base_stat: 24},
-                    {base_stat: 48},
-                    {base_stat: 80},
-                    {base_stat: 35},
-                    {base_stat: 158},
-                    {base_stat: 139},
-                ],
-            isFavorite: false,
-            lvl: 37,
-            hp: 158,
-            hpMax: 158
-        },
-        {
-            name: "mew",
-            type: "psychic",
-            stats:
-                [
-                    {base_stat: 88},
-                    {base_stat: 24},
-                    {base_stat: 88},
-                    {base_stat: 80},
-                    {base_stat: 35},
-                    {base_stat: 457},
-                    {base_stat: 428},
-                ],
-            isFavorite: false,
-            lvl: 87,
-            hp: 468,
-            hpMax: 468
-        },
-        {
-            name: "snorlax",
-            type: "normal",
-            stats:
-                [
-                    {base_stat: 88},
-                    {base_stat: 24},
-                    {base_stat: 48},
-                    {base_stat: 80},
-                    {base_stat: 35},
-                    {base_stat: 489},
-                    {base_stat: 287},
-                ],
-            isFavorite: false,
-            lvl: 68,
-            hp: 385,
-            hpMax: 385
-        },
-        {
-            name: "blastoise",
-            type: "water",
-            stats:
-                [
-                    {base_stat: 88},
-                    {base_stat: 24},
-                    {base_stat: 48},
-                    {base_stat: 80},
-                    {base_stat: 35},
-                    {base_stat: 348},
-                    {base_stat: 27},
-                ],
-            isFavorite: false,
-            lvl: 68,
-            hp: 385,
-            hpMax: 385
-        }
-    ];
 
     const pushNew = () => {
         model.push(newPokemon);
@@ -165,9 +74,9 @@ const TeamPokemon = () => {
                 </div>
             }
             <div className="navMenuFake">
-                <img className="menuItem"img src="http://www.pokestadium.com/sprites/xy/charizard.gif" alt=""/>
-                <img className="menuItem" src="http://www.pokestadium.com/sprites/xy/charizard.gif" alt=""/>
-                <img className="menuItem" src="http://www.pokestadium.com/sprites/xy/charizard.gif" alt=""/>
+                <img className="menuItem" src="http://www.pokestadium.com/sprites/xy/charizard.gif" alt="charizard"/>
+                <img className="menuItem" src="http://www.pokestadium.com/sprites/xy/charizard.gif" alt="charizard"/>
+                <img className="menuItem" src="http://www.pokestadium.com/sprites/xy/charizard.gif" alt="charizard"/>
             </div>
         </div>
     )
