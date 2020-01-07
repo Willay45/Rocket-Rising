dc519305ef4c8c68cc5ad78df43f0dcc511f35b1
Ugo Angeletti <ugo.angeletti@yahoo.fr>
2020-01-07 16:47:23 +0100

    fix text tutorial

--- src/components/scenes/TutorialSceneManager.jsx
+++ src/components/scenes/TutorialSceneManager.jsx
@@ -11,27 +11,20 @@ const TutorialSceneManager = () => {
         MusicService.play("tutorial");
     });
 
-const messages = [`Welcome to the Team Rocket, young rookie !`,
-    `Well you may know our plan, I repeat for you :`,
-    `We want to control the world and all the pokemons by the way !`,
-    `And for that, we made a new powerful pokeball : The ShadowBall !`,
-    `It's allow you to capture pokemons, whatever they are wild or belong to another trainer.`,
-    `Use it for steal a maximum of citizen's pokemons.`,
-    `You past with succes the selections, but you still have to do your proofs !`,
-    `Your first mission will be to bring back a Poliwag.`,
-    `It looks like than a girl on the beach has already that pokemon. You know what to do…`,
-    `However, it's possible than she try to defend herself…`,
-    ` What, you don't have any pokemon ?`,
-    `Well… Choose one among this three of them :`,
-    `(Starter choice screen)`,
-    `Good. Come back with the Stary when it's done.`,
-    `Good luck, and don't get caught by Looker or another policeman.`,
-    `(Click here to open the map and go to the beach)`,
-    `Well done, you have fulfill your mission has expected.`,
-    `Now, go outside, and be sure than the Team Rocket is feared by everyone !`,
-    `If you need something, come back here to buy some objects for your stuff.`
-
+    const messages = [`Welcome to the Team Rocket, young rookie !
+    Well you may know our plan, I repeat for you :`,
+    `We want to control the world and all the pokemons by the way !
+    For that, we made a new powerful pokeball : The ShadowBall !`,
+    `It's allow you to capture any pokemons, even those belong to another trainer.
+    Use it for steal the maximum of them.`,
+    `You passed the selections, but you still have to do your proofs !
+    Your first mission will be to bring back a Poliwag.`,
+    `It looks like than a girl on the beach has this pokemon. You know what to do…
+    However, it's possible than she try to defend herself…`,
+    `What, you don't have any pokemon ?
+    Well… Choose one among this three of them :`
 ];
+
 
 
 
