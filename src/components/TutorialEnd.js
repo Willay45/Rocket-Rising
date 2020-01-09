ebc572ea9def35a5507cfdc7e0375875cff0df20
eloise87 <eloise.marie@laposte.net>
2020-01-09 11:25:50 +0100

    last css fix

--- src/components/TutorialEnd.js
+++ src/components/TutorialEnd.js
@@ -4,14 +4,12 @@ import './TutorialEnd.css'
 
 const TutorialEnd = () => {
 
-    return(
+    return (
         <div className="tutorialEndContainer">
-            <div className="titleMap">
-                <h1>Go !</h1>
-            </div>
-            <div className="backgroundImage">
+            <div className="titleMap"/>
 
-            </div>
+            <div className="backgroundImage"/>
+
             <div className="tipsEnd">
                 <h3> Click on the map ! </h3>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/b/b0/Down_red_arrow.png" alt="red arrow"/>
